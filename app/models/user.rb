e45cbfd34caf712857f6cd1dc39f73e2eb95b85a
kykloBod <tkachukevgeniy2004@gmail.com>
2023-11-17 13:04:49 +0300

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+              
+  enum role: [:user, :seller, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, length: {maximum: 20}
+  validates :username, length: {maximum: 20}
+  validates :last_name, length: {maximum: 20}
+  def set_default_role
+    self.role ||= :user
+  end
+end
