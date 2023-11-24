62825caecc6d69b489330781be8417ea9c742270
kykloBod <tkachukevgeniy2004@gmail.com>
2023-11-24 10:51:22 +0300

    Update products_controller.rb
    new function: show, new, create, edit, update, destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,47 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Товар создан'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Товар обновлен'
+    else
+      render :edit
+    end
+  end 
+    def destroy
+    @product.destroy
+    redirect_to products_url, notice: 'Товар удален'
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
+
+ def product_params
+  params.require(:product).permit(:name, :description, :price, :rating, :avatar)
+ end
+
