691a77746db535b15e8fa9ac6a2343f1d101a9e7
kykloBod <tkachukevgeniy2004@gmail.com>
2023-12-01 12:47:10 +0300

    validates for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,22 @@
 class Product < ApplicationRecord
         mount_uploader :avatar, AvatarUploader
+        validates :price, presence: true,
+                          length: {maximum: 15},
+                          numericality: {
+                            only_integer: true,
+                            greater_than_or_equal_to: 1,
+                            less_than_or_equal_to: 10000000
+                          }
+
+                            validates :rating, presence: true,
+                            length: {maximum: 2},
+                            numericality: {
+                              only_integer: true,
+                              greater_than_or_equal_to: 1,
+                              less_than_or_equal_to: 10
+                            }
+
+                            validates :name, presence: true,
+                            length: {maximum: 2}
+                            
 end
