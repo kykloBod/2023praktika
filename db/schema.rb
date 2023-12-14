d6692a8b940bf9811d9e9e721a5f925cc33cc4b0
kykloBod <tkachukevgeniy2004@gmail.com>
2023-12-14 18:14:07 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_122735) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_14_113435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orderables", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orderables_on_cart_id"
+    t.index ["product_id"], name: "index_orderables_on_product_id"
+  end
 
   create_table "product_tests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,13 +37,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_122735) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.decimal "price"
+    t.integer "price"
     t.integer "rating"
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,5 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_122735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "products", "users"
+  add_foreign_key "orderables", "carts"
+  add_foreign_key "orderables", "products"
 end
